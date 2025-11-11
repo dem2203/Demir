8302ecde68338af75d1a1afea03c7d917a4bba3c
dem2203 <idemir2203@gmail.com>
2025-11-11 21:48:21 +0100

    Create exchange_flow_layer.py

---
+++ layers/exchange_flow_layer.py
@@ -0,0 +1,11 @@
+```python
+class ExchangeFlowLayer:
+    def analyze(self):
+        return {
+            'flows': 'monitoring',
+            'inflow_pressure': 'medium',
+            'signal': 'neutral'
+        }
+
+exchange_flow = ExchangeFlowLayer()
+```
