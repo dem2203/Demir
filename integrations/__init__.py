f56297838276e983ef3bb9aec75e1755be8fc07c
dem2203 <idemir2203@gmail.com>
2025-11-19 23:32:47 +0100

    Update __init__.py

--- integrations/__init__.py
+++ integrations/__init__.py
@@ -1,15 +1,1 @@
 """Integration module - API connections"""
-
-from .multi_exchange_api import MultiExchangeAggregator
-from .macro_data_aggregator import MacroAggregator
-from .sentiment_aggregator import SentimentAggregator
-from .defi_and_onchain_api import CoingglassAPI, DexCheckAPI, OpenSeaAPI
-
-__all__ = [
-    'MultiExchangeAggregator',
-    'MacroAggregator',
-    'SentimentAggregator',
-    'CoingglassAPI',
-    'DexCheckAPI',
-    'OpenSeaAPI'
-]
