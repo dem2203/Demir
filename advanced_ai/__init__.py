117ae1f6b4eb833fa0ed90d3a90886a5220aef2f
dem2203 <idemir2203@gmail.com>
2025-11-19 23:12:39 +0100

    Update __init__.py

--- advanced_ai/__init__.py
+++ advanced_ai/__init__.py
@@ -1,13 +1,8 @@
 """Advanced AI module"""
 
-from .causality_inference import CausalInference
-from .lstm_trainer import LSTMTrainer
-from .regime_detector import RegimeDetector
-from .layer_optimizer import LayerOptimizer
-
-__all__ = [
-    'CausalInference',
-    'LSTMTrainer',
-    'RegimeDetector',
-    'LayerOptimizer'
-]
+# Import only if files exist
+try:
+    from .signal_engine_integration import SignalGroupOrchestrator
+    __all__ = ['SignalGroupOrchestrator']
+except ImportError:
+    __all__ = []
