bc8277aa9a31a55fc0355f820b14990ff789cf80
dem2203 <idemir2203@gmail.com>
2025-11-19 23:15:45 +0100

    Update __init__.py

--- advanced_ai/__init__.py
+++ advanced_ai/__init__.py
@@ -1,8 +1,1 @@
 """Advanced AI module"""
-
-# Import only if files exist
-try:
-    from .signal_engine_integration import SignalGroupOrchestrator
-    __all__ = ['SignalGroupOrchestrator']
-except ImportError:
-    __all__ = []
